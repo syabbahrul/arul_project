2b53c4bb6065a2cb9bc38e8d8369ea5b0ca25278
fuad002 <77085017+fuad002@users.noreply.github.com>
2021-01-07 10:54:00 +0700

    Update helloworld.java
    kurang panjang kata" nya , sama kurang berbobot

--- helloworld.java
+++ helloworld.java
@@ -7,5 +7,7 @@ public helloworld{
     System.out.println("owh iya lah.eh entar anterin aq ya");
     System.out.println("Gampang boss");
     System.out.println("udah kah wing");
+     System.out.println("fuad ganteng gk?");
+     System.out.println("Kata syabahrul: ihh ganteng banget fuad, wkwkwk");
   }
 }
