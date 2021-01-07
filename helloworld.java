3519ae780bc352140ea75e851b5b09086ecbd138
syabbahrul <77084816+syabbahrul@users.noreply.github.com>
2021-01-07 10:33:18 +0700

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,7 @@
 public helloworld{
   public static void main(){
     System.out.println("Hello World");
-    System.out.println("Selamat Pagi Dunia Tipu-Tipu");    
+    System.out.println("Selamat Pagi Dunia Tipu-Tipu");
+    System.out.println("ngapain lu");
   }
 }
