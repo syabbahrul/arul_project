cd7f2190b61193867d029a7b12c169b2db4bdc95
yudiansyah25 <77084763+yudiansyah25@users.noreply.github.com>
2021-01-19 08:55:34 +0700

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -9,5 +9,6 @@ public helloworld{
     System.out.println("udah kah wing");
      System.out.println("fuad ganteng gk?");
      System.out.println("Kata syabahrul: ihh ganteng banget fuad, wkwkwk");
+    System.out.println("Kata syabahrul: ihh kan anu nya fuad tari kan , wkwkwkwk");
   }
 }
